145fb0f1396c9974da53269e1ac3fe23eb5f2ad4
Pepperoner <boris.lahutin@gmail.com>
2017-02-12 16:34:40 +0200

    Module2

--- Module2/homework/TaskOne.java
+++ Module2/homework/TaskOne.java
@@ -1,24 +1,39 @@
 package Module2.homework;
 
-import java.util.Random;
+import java.util.Formatter;
 
 public class TaskOne {
     public static void main(String[] args) {
-        int[] array = new int[10];
-        System.out.println("Generated array ");
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * 40 - 20);
-            System.out.print(array[i] + " ");
+        int[] intArray = new int[10];
+        System.out.println("Generated array of integers ");
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = (int) (Math.random() * 40 - 20);
+            System.out.print(intArray[i] + " ");
         }
         System.out.println();
-        System.out.println("Result sum" + ' ' + Utility.sum(array));
-        System.out.println("Min = " + Utility.min(array));
-        System.out.println("Max = " + Utility.max(array));
-        System.out.println("Max positive = " + Utility.maxPositive(array));
-        System.out.println("Multiplication = " + Utility.multiplication(array));
-        System.out.println("Modulus of first element = " + Utility.modulusOfFirstElement(array));
-        System.out.println("Modulus of last element = " + Utility.modulusOfLastElement(array));
-        System.out.println("Second largest = " + Utility.secondLargest(array));
+        double[] doubleArray = new double[10];
+        System.out.println("Generated array of doubles ");
+        for (int i = 0; i < doubleArray.length; i++) {
+            doubleArray[i] = (Math.random() * 40 - 20);
+            System.out.print(doubleArray[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Result sum of integers " + Utility.sum(intArray));
+        System.out.println("Result sum of doubles " + Utility.sum(doubleArray));
+        System.out.println("Min value in int array = " + Utility.min(intArray));
+        System.out.println("Min value in double array = " + Utility.min(doubleArray));
+        System.out.println("Max value in int array = " + Utility.max(intArray));
+        System.out.println("Max value in double array = " + Utility.max(doubleArray));
+        System.out.println("Max positive of int array = " + Utility.maxPositive(intArray));
+        System.out.println("Max positive of double array = " + Utility.maxPositive(doubleArray));
+        System.out.println("Multiplication of int array = " + Utility.multiplication(intArray));
+        System.out.println("Multiplication of double array = " + Utility.multiplication(doubleArray));
+        System.out.println("Modulus of first element = " + Utility.modulusOfFirstElement(intArray));
+        System.out.println("Modulus of first element = " + Utility.modulusOfFirstElement(doubleArray));
+        System.out.println("Modulus of last element = " + Utility.modulusOfLastElement(intArray));
+        System.out.println("Modulus of last element = " + Utility.modulusOfLastElement(doubleArray));
+        System.out.println("Second largest value in array of integers = " + Utility.secondLargest(intArray));
+        System.out.println("Second largest value in array of doubles = " + Utility.secondLargest(doubleArray));
     }
 
 }
--- Module2/homework/Utility.java
+++ Module2/homework/Utility.java
@@ -9,6 +9,13 @@ public class Utility {
         }
         return sum;
     }
+    public static double sum(double array[]) {
+        double sum = 0;
+        for (double anArray : array) {
+            sum += anArray;
+        }
+        return sum;
+    }
     public static int min(int array[]) {
         int min = array[0];
         for (int i = 1; i < array.length; i++) {
@@ -17,8 +24,25 @@ public class Utility {
         }
         return min;
     }
+    public static double min(double array[]) {
+        double min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min)
+                min = array[i];
+        }
+        return min;
+    }
     public static int max(int array[]) {
         int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+    public static double max(double array[]) {
+        double max = array[0];
         for (int i = 1; i < array.length; i++) {
             if (array[i] > max) {
                 max = array[i];
@@ -35,9 +59,27 @@ public class Utility {
             }
         return maxPositive;
     }
+    public static double maxPositive(double array[]) {
+        double maxPositive = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if ((array[i] > maxPositive) && (array[i] >= 0 )) {
+                maxPositive = array[i];
+            }
+        }
+        return maxPositive;
+    }
     public static int multiplication(int array[]) {
         int multiplication = 1;
         for (int anArray : array) {
+            if (anArray != 0) {
+                multiplication *= Math.abs(anArray);
+            }
+        }
+        return multiplication;
+    }
+    public static double multiplication(double array[]) {
+        double multiplication = 1;
+        for (double anArray : array) {
             if (anArray != 0) {
                 multiplication *= Math.abs(anArray);
             }
@@ -48,13 +90,36 @@ public class Utility {
         int modulus = Math.abs(array[0]);
         return modulus;
     }
+    public static double modulusOfFirstElement(double array[]) {
+        double modulus = Math.abs(array[0]);
+        return modulus;
+    }
     public static int modulusOfLastElement(int array[]) {
         int modulus = Math.abs(array[array.length - 1]);
+        return modulus;
+    }
+    public static double modulusOfLastElement(double array[]) {
+        double modulus = Math.abs(array[array.length - 1]);
         return modulus;
     }
     public static int secondLargest(int array[]) {
         int temp;
         int secondLargest;
+        for (int i = 0; i < array.length; i++){
+            for (int j = 1; j < (array.length-i); j++) {
+                if (array[j-1] > array[j]) {
+                    temp = array[j-1];
+                    array[j-1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        secondLargest = array[array.length - 2];
+        return secondLargest;
+    }
+    public static double secondLargest(double array[]) {
+        double temp;
+        double secondLargest;
         for (int i = 0; i < array.length; i++){
             for (int j = 1; j < (array.length-i); j++) {
                 if (array[j-1] > array[j]) {
