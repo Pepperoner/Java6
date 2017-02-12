391a0e9cae502f0ac21b2ca31b763a40c72fc876
Pepperoner <boris.lahutin@gmail.com>
2017-02-12 17:05:06 +0200

    Module2

--- Module2/homework/TaskOne.java
+++ Module2/homework/TaskOne.java
@@ -21,22 +21,30 @@ public class TaskOne {
             System.out.print(" ");
         }
         System.out.println();
-        System.out.println("Result sum of integers " + Utility.sum(intArray));
-        System.out.println("Result sum of doubles " + Utility.sum(doubleArray));
+        System.out.println("Result sum of integers = " + Utility.sum(intArray));
+        System.out.print("Result sum of doubles = " );
+        System.out.format("%.2f%n",Utility.sum(doubleArray));
         System.out.println("Min value in int array = " + Utility.min(intArray));
-        System.out.println("Min value in double array = " + Utility.min(doubleArray));
+        System.out.print("Min value in double array = ");
+        System.out.format("%.2f%n",Utility.min(doubleArray));
         System.out.println("Max value in int array = " + Utility.max(intArray));
-        System.out.println("Max value in double array = " + Utility.max(doubleArray));
+        System.out.print("Max value in double array = ");
+        System.out.format("%.2f%n",Utility.max(doubleArray));
         System.out.println("Max positive of int array = " + Utility.maxPositive(intArray));
-        System.out.println("Max positive of double array = " + Utility.maxPositive(doubleArray));
+        System.out.print("Max positive of double array = ");
+        System.out.format("%.2f%n",Utility.maxPositive(doubleArray));
         System.out.println("Multiplication of int array = " + Utility.multiplication(intArray));
-        System.out.println("Multiplication of double array = " + Utility.multiplication(doubleArray));
+        System.out.print("Multiplication of double array = ");
+        System.out.format("%.2f%n",Utility.multiplication(doubleArray));
         System.out.println("Modulus of first element = " + Utility.modulusOfFirstElement(intArray));
-        System.out.println("Modulus of first element = " + Utility.modulusOfFirstElement(doubleArray));
+        System.out.print("Modulus of first element = ");
+        System.out.format("%.2f%n",Utility.modulusOfFirstElement(doubleArray));
         System.out.println("Modulus of last element = " + Utility.modulusOfLastElement(intArray));
-        System.out.println("Modulus of last element = " + Utility.modulusOfLastElement(doubleArray));
+        System.out.print("Modulus of last element = ");
+        System.out.format("%.2f%n",Utility.modulusOfLastElement(doubleArray));
         System.out.println("Second largest value in array of integers = " + Utility.secondLargest(intArray));
-        System.out.println("Second largest value in array of doubles = " + Utility.secondLargest(doubleArray));
+        System.out.print("Second largest value in array of doubles = ");
+        System.out.format("%.2f%n",Utility.secondLargest(doubleArray));
     }
 
 }
