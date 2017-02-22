d8ab53d0c0af2cfbc12fba5956674ad8eb563240
Pepperoner <boris.lahutin@gmail.com>
2017-02-22 14:20:31 +0200

    Module2

--- module3/homework/module3/homework/task2/Adder.java
+++ module3/homework/module3/homework/task2/Adder.java
@@ -1,9 +1,9 @@
 package module3.homework.module3.homework.task2;
 
 public class Adder extends Arithmetic {
-    public Adder(Integer a1,Integer b1) {
-        a = a1;
-        b = b1;
+    public Adder(Integer a,Integer b) {
+        this.a = a;
+        this.b = b;
     }
     public static boolean check (Integer a, Integer b){
         if (a>=b)
