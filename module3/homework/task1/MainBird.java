86923bbb476c9cb42bc36814d0b5efeb6105a5f4
Pepperoner <boris.lahutin@gmail.com>
2017-02-26 19:49:24 +0200

    Module3

---
+++ module3/homework/task1/MainBird.java
@@ -0,0 +1,12 @@
+package module3.homework.task1;
+
+
+public class MainBird {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.sing("I am walking");
+        bird.sing("I am flying");
+        bird.sing("I am singing");
+        bird.sing("I am Bird");
+    }
+}
