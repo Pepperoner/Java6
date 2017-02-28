c751cd47b37e412863276eb0906a26a1a2eecc66
Pepperoner <boris.lahutin@gmail.com>
2017-02-28 12:35:26 +0200

    Module4

--- module4/homework/BankSystemImpl.java
+++ module4/homework/BankSystemImpl.java
@@ -4,54 +4,54 @@ public class BankSystemImpl implements BankSystem {
 
     @Override
     public void withdrawOfUser(User user, int amount) {
-        if (getLimitOfWithdrawal() > 0 && amount > getLimitOfWithdrawal()) {
+        double userBalance = user.getBalance();
+        Bank userBank = user.getBank();
+        int limitWithdrawal = userBank.getLimitOfWithdrawal();
+        if (limitWithdrawal > 0 && amount > limitWithdrawal) {
             System.out.println("Withdraw is unable: requested amount is out of bank limit.");
         }
-        System.out.println("Withdraw " + amount + currency.toString());
-        System.out.println("Commission is: " + getCommission(amount) + "%");
-        amount += (int) (getCommission(amount) * amount / 100.0);
+        int commissionPercent = userBank.getCommission(amount);
+        user.setBalance(userBalance - amount -(amount * ((double) commissionPercent/100)));
+        System.out.println("Your balance " + user.getName() + " is:" + user.getBalance() + " with commission " + commissionPercent + "%");
 
         if (user.getBalance() - amount < 0) {
             System.out.println("Withdraw is unable: not enough funds on you balance");
         }
-        user.setBalance(user.getBalance() - amount);
-        System.out.println("Your balance was changed.\n");
+        System.out.println("Your balance was changed");
     }
 
     @Override
     public void fundUser(User user, int amount){
-        System.out.println("Funding " + amount + currency.toString() + " to " + user.getName());
-        if (getLimitOfFunding() > 0 && amount > getLimitOfFunding()){
+        double userBalance = user.getBalance();
+        Bank userBank = user.getBank();
+        int limitOfFunding = userBank.getLimitOfFunding();
+        if (limitOfFunding > 0 && amount > limitOfFunding){
             System.out.println("Funding is unable: you're trying to fund more then bank limit");
         }
-        user.setBalance(user.getBalance() + amount);
-        System.out.println("Your balance was changed.\n");
+        user.setBalance(userBalance + amount);
+        System.out.println("New balance of user "+user.getName()+" is: "+user.getBalance());
     }
 
     @Override
     public void transferMoney(User fromUser, User toUser, int amount){
+        double fromUserBalance = fromUser.getBalance();
+        double toUserBalance = toUser.getBalance();
+        if (amount>fromUserBalance){
+            System.out.println(fromUser.getName()+" your balance in not enough for this transaction");
+        }
         if (fromUser.equals(toUser)){
             System.out.println("You are trying to transfer money to yourself.\nSuch transfer is impossible");
         }
-
-        double toUserCurrentBalance = toUser.getBalance(), fromUserCurrentBalance = fromUser.getBalance();
         System.out.println("Transferring money...");
         fundUser(toUser,amount);
-        withdrawOfUser(fromUser,amount);
-
-        if (toUser.getBalance() == toUserCurrentBalance || fromUser.getBalance() == fromUserCurrentBalance){
-            System.out.println("Transferring is impossible");
-            toUser.setBalance(toUserCurrentBalance);
-            fromUser.setBalance(fromUserCurrentBalance);
-        }
+        System.out.println(fromUser.getName()+" sent "+amount+" to "+toUser.getName()+". "+fromUser.getName()+" your balance was: "+ fromUserBalance);
+        System.out.println(fromUser.getName()+" balance after transfer is: "+(fromUserBalance-amount)+". "+toUser.getName()+" now your balance is: "+ (toUserBalance + amount));
         System.out.println("Success. Your transfer is complete\n");
     }
 
     @Override
     public void paySalary(User user){
-        int amount = user.getSalary();
-        System.out.println("Earn " + user.getName() + "salary. Amount :" + amount + currency.toString());
-        user.setBalance(user.getBalance() + amount);
-        System.out.println("Your balance was changed.\n");
+        user.setBalance(user.getBalance() + user.getSalary());
+        System.out.println(user.getName() + "Your balance with salary is: " + user.getBalance());
     }
 }
--- module4/homework/TestOfBankSystem.java
+++ module4/homework/TestOfBankSystem.java
@@ -3,25 +3,29 @@ package module4.homework;
 public class TestOfBankSystem {
 
     public static void main(String[] args) {
-
-        Bank bank = new USBank();
-        Bank bank2 = new EUBank();
-        Bank bank3 = new ChinaBank();
+        BankSystemImpl bankSystem = new BankSystemImpl();
+        Bank bank = new USBank(10,"USA",Currency.USD,200,2000.0,20,1000000);
+        Bank bank2 = new EUBank(15,"France",Currency.EUR,250,1500.0,15,1500000);
+        Bank bank3 = new ChinaBank(20,"Beijing",Currency.USD,300,1000,30,500000);
         User user = new User(1,"Boris",1000,1,"AT&T",3000,bank);
-        User user1 = new User(2,"Mark",1000,2,"Google",3500,bank);
+        User user1 = new User(2,"Mark",2000,2,"Google",3500,bank);
         User user2 = new User(3,"Alex",3000,3,"Porshe",4000,bank2);
         User user3 = new User(4,"Tanya",3000,4,"Yahoo",2000,bank2);
         User user4 = new User(5,"Jet Lee",1500,5,"Steam",1000,bank3);
         User user5 = new User(6,"Jacky",1500,6,"Samsung",1500,bank3);
 
         System.out.println(user);
-        bank.withdrawOfUser(user,300);
+        bankSystem.withdrawOfUser(user,200);
+        System.out.println();
+        System.out.println(user1);
+        bankSystem.fundUser(user1,2000);
+        System.out.println();
         System.out.println(user);
-        bank.paySalary(user);
-        System.out.println(user);
-        bank.fundUser(user,150);
-        System.out.println(user);
-        bank.transferMoney(user1,user,750);
-        System.out.println(user);
+        System.out.println(user1);
+        bankSystem.transferMoney(user1,user,1500);
+        System.out.println();
+        System.out.println(user4);
+        bankSystem.paySalary(user4);
+
     }
 }
